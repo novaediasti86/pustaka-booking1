f0c7b180e73e58ea594b682afe045ee36b0b5ab8
novaediasti86 <novaediasti86@gmail.com>
2022-05-08 23:56:54 +0700

    push Web 3 Per 6

---
+++ application/views/templates/sidebar.php
@@ -0,0 +1,233 @@
+<!-- Sidebar -->
+        <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
+
+            <!-- Sidebar - Brand -->
+            <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
+                <div class="sidebar-brand-icon rotate-n-15">
+                    <i class="fas fa-book"></i>
+                </div>
+                <div class="sidebar-brand-text mx-3">Pustaka Booking1</div>
+            </a>
+
+            <!-- Divider -->
+            <hr class="sidebar-divider">
+
+
+
+            <!-- Looping Menu-->
+                <div class="sidebar-heading">
+                    Home
+                </div>
+                    <li class="nav-item active">
+                        <!-- Nav Item - Dashboard -->
+                        <li class="nav-item">
+                            <a class="nav-link pb-0" href="<?= base_url('admin'); ?>">
+                                <!--<i class="fa fa-fw fa book"></i>-->
+                                <i class="fas fa-tachometer-alt"></i>
+                                <span>Dashboard</span></a>
+                        </li>
+                    </li>
+
+                <!-- Divider -->
+                <hr class="sidebar-divider mt-3">
+
+                <!-- Heading -->
+                <div class="sidebar-heading">
+                    Master Data
+                </div>
+                    <!-- Nav Item - Dashboard -->
+                    <li class="nav-item active">
+                        <!-- Nav Item - Dashboard -->
+                        <li class="nav-item">
+                            <a class="nav-link pb-0" href="<?= base_url('buku/kategori'); ?>">
+                              <i class="fas fa-book"></i>
+                               <!--  <i class="fa fa-fw fa book"></i>-->
+                                <span>Kategori Buku</span></a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link pb-0" href="<?= base_url('buku'); ?>">
+                              <!--<i class="fa fa-fw fa book"></i>-->
+                                    <i class="fas fa-address-book"></i>
+                                <span>Data Buku</span></a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link pb-0" href="<?= base_url('user/anggota'); ?>">
+                            <i class="fas fa-users"></i>
+
+                                <span>Data Anggota</span></a>
+                        </li>
+                    </li>
+                <!-- Divider -->
+                <hr class="sidebar-divider mt-3">
+
+                <!--Heading-->
+                <div class="sidebar-heading">
+                    Transaksi
+                </div>
+                    <!--Nav Item - Dashboard -->
+                    <li class="nav-item Active">
+                        <!-- Nav Item - Dashboard -->
+                        <li class="nav-item">
+                            <a class="navlink pb-0" href="?=base_url('pinjam'); ?>">
+                                <i class="fa fa-fw fa-shopping-cart"></i>
+                                <span>Data Peminjaman</span></a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link pb-0" href=" <?=base_url('pinjam/daftarBooking');?>">
+                            <i class="fa fa-fw fa-list"></i>
+                            <span>Data Booking</span></a>
+                        </li>
+
+                    </li>
+                    <!-- Divider -->
+                    <hr class="sidebar-divider mt-3"><?php
+
+class Autentifikasi extends CI_Controller
+{
+
+    public function index()
+    {
+        //jika statusnya sudah login, maka tidak bisa mengakses halaman login alias dikembalikan ke tampilan user
+        if($this->session->userdata('email')){
+            redirect('admin');
+        }
+
+        $this->form_validation->set_rules('email', 'Alamat Email', 'required|trim|valid_email', [
+            'required' => 'Email Harus diisi!!',
+            'valid_email' => 'Email Tidak Benar!!'
+        ]);
+        $this->form_validation->set_rules('password', 'Password', 'required|trim', [
+            'required' => 'Password Harus diisi'
+        ]);
+        if ($this->form_validation->run() == false) {
+            $data['judul'] = 'Login';
+            $data['user'] = '';
+            //kata 'login' merupakan nilai dari variabel judul dalam array $data dikirimkan ke view aute_header
+            $this->load->view('templates/aute_header', $data);
+            $this->load->view('autentifikasi/login');
+            $this->load->view('templates/aute_footer');
+        } else {
+            $this->_login();
+        }
+    }
+
+    private function _login()
+    {
+        $email = htmlspecialchars($this->input->post('email', true));
+        $password = $this->input->post('password', true);
+
+        $user = $this->ModelUser->cekData(['email' => $email])->row_array();
+
+        //jika usernya ada
+        if ($user) {
+            //jika role_id : 1
+            
+            //jika user sudah aktif
+            if ($user['is_active'] == 1) {
+                //cek password
+                if (password_verify($password, $user['password'])) {
+                    $data = [
+                        'email' => $user['email'],
+                        'role_id' => $user['role_id']
+                    ];
+
+                    $this->session->set_userdata($data);
+                    redirect('admin');
+                } else {
+                    $this->session->set_flashdata('pesan', '<div class="alert alert-danger alert-message" role="alert">Password salah!!</div>');
+                    redirect('autentifikasi');
+                }
+            } else {
+                $this->session->set_flashdata('pesan', '<div class="alert alert-danger alert-message" role="alert">User belum diaktifasi!!</div>');
+                redirect('autentifikasi');
+            }
+        } else {
+            $this->session->set_flashdata('pesan', '<div class="alert alert-danger alert-message" role="alert">Email tidak terdaftar!!</div>');
+            redirect('autentifikasi');
+        }
+    }
+
+    public function registrasi()
+    {
+        if ($this->session->userdata('email')) {
+            redirect('user');
+        }
+        //membuat rule untuk inputan nama agar tidak boleh kosong dengan membuat pesan error dengan
+        //bahasa sendiri yaitu 'Nama Belum diisi'
+        $this->form_validation->set_rules('nama', 'Nama Lengkap', 'required', [
+            'required' => 'Nama Belum diis!!'
+        ]);
+        //membuat rule untuk inputan email agar tidak boleh kosong, tidak ada spasi, format email harus valid
+        //dan email belum pernah dipakai sama user lain dengan membuat pesan error dengan bahasa sendiri
+        //yaitu jika format email tidak benar maka pesannya 'Email Tidak Benar!!'. jika email belum diisi,
+        //maka pesannya adalah 'Email Belum diisi', dan jika email yang diinput sudah dipakai user lain,
+        //maka pesannya 'Email Sudah dipakai'
+        $this->form_validation->set_rules('email', 'Alamat Email', 'required|trim|valid_email|is_unique[user.email]', [
+            'valid_email' => 'Email Tidak Benar!!',
+            'required' => 'Email Belum diisi!!',
+            'is_unique' => 'Email Sudah Terdaftar!'
+        ]);
+        //membuat rule untuk inputan password agar tidak boleh kosong, tidak ada spasi, tidak boleh kurang dari
+        //dari 3 digit, dan password harus sama dengan repeat password dengan membuat pesan error dengan
+        //bahasa sendiri yaitu jika password dan repeat password tidak diinput sama, maka pesannya
+        //'Password Tidak Sama'. jika password diisi kurang dari 3 digit, maka pesannya adalah
+        //'Password Terlalu Pendek'.
+        $this->form_validation->set_rules('password1', 'Password', 'required|trim|min_length[3]|matches[password2]', [
+            'matches' => 'Password Tidak Sama!!',
+            'min_length' => 'Password Terlalu Pendek'
+        ]);
+        $this->form_validation->set_rules('password2', 'Repeat Password', 'required|trim|matches[password1]');
+        //jika jida disubmit kemudian validasi form diatas tidak berjalan, maka akan tetap berada di
+        //tampilan registrasi. tapi jika disubmit kemudian validasi form diatas berjalan, maka data yang
+        //diinput akan disimpan ke dalam tabel user
+        if ($this->form_validation->run() == false) {
+            $data['judul'] = 'Registrasi Member';
+            $this->load->view('templates/aute_header', $data);
+            $this->load->view('autentifikasi/registrasi');
+            $this->load->view('templates/aute_footer');
+        } else {
+            $email = $this->input->post('email', true);
+            $data = [
+                'nama' => htmlspecialchars($this->input->post('nama', true)),
+                'email' => htmlspecialchars($email),
+                'image' => 'default.jpg',
+                'password' => password_hash($this->input->post('password1'), PASSWORD_DEFAULT),
+                'role_id' => 2,
+                'is_active' => 1,
+                'tanggal_input' => time()
+            ];
+
+            $this->ModelUser->simpanData($data); //menggunakan model
+
+            $this->session->set_flashdata('pesan', '<div class="alert alert-success alert-message" role="alert">Selamat!! akun member anda sudah dibuat. Silahkan Aktivasi Akun anda</div>');
+            redirect('autentifikasi');
+        }
+    }
+
+    public function logout()
+    {
+        $this->session->unset_userdata('email');
+        $this->session->unset_userdata('role_id');
+
+        $this->session->set_flashdata('pesan', '<div class="alert alert-success alert-message" role="alert">Anda telah logout!!</div>');
+        redirect('autentifikasi');
+    }
+
+    public function blok()
+    {
+        $this->load->view('autentifikasi/blok');
+    }
+
+    public function gagal()
+    {
+        $this->load->view('autentifikasi/gagal');
+    }
+}
+
+            <!-- Sidebar Toggler (Sidebar) -->
+            <div class="text-center d-none d-md-inline">
+                <button class="rounded-circle border-0" id="sidebarToggle"></button>
+            </div>
+
+        </ul>
+        <!-- End of Sidebar --   >
---
+++ application/views/v_about.php
@@ -0,0 +1,26 @@
+<section>
+	<h1><?php echo $judul ?></h1>
+	<h4>Nama</h4>
+	<ul type="disc">
+		<li>Nama Depan : Natusha</li>
+		<li>Nama Belakang : Hazel</li>
+	</ul>
+	<br>
+	<h4>Alamat</h4>
+	<ul type="none">
+		<li> Jalan Ciledug Raya No. 168 Pesanggrahan</li>
+	</ul>
+	
+	<h4>Tempat Lahir</h4>
+	<ul type="none">
+		<li>Yogyakarta</li>
+	</ul>
+
+	<h4>Olah Raga Favorit</h4>
+	<ul type="square">
+		<li>Basket</li>
+		<li>Judo</li>
+	</ul>
+	
+
+</section>
